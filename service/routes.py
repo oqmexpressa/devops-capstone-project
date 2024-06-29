5097c3c5ba85fd842948abf0a2461b8262239d2d
Alber <alber@oqmexpressa.io>
2024-06-29 09:45:16 -0400

    fix blank lines

--- service/routes.py
+++ service/routes.py
@@ -57,10 +57,10 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -77,7 +77,6 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route('/accounts/<int:account_id>', methods=['GET'])
 def get_accounts(account_id):
     "Read an account."
@@ -88,14 +87,15 @@ def get_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     "Update an Account."
-    
     app.logger.info(f"Request to update an Account with id: {account_id}")
     account = Account.find(account_id)
     if not account:
